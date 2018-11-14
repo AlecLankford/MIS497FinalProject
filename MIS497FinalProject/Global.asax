37ac18d201dbd1aa29f3e15a4006e804767d9a6d
Alec Lankford <adlankford@crimson.ua.edu>
2018-11-14 14:52:28 -0600

    Add project files.

---
+++ MIS497FinalProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MIS497FinalProject.MvcApplication" Language="C#" %>
---
+++ MIS497FinalProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MIS497FinalProject.Startup))]
+namespace MIS497FinalProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
