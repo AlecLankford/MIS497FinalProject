fc2186b6e54e5021de39a6af7e08875b7b504389
Alec Lankford <adlankford@crimson.ua.edu>
2018-11-29 12:26:21 -0600

    Pie Chart

---
+++ MIS497FinalProject/Controllers/GraphsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MIS497FinalProject.Models;
+
+namespace MIS497FinalProject.Controllers
+{
+    public class GraphsController : Controller
+    {
+        // GET: Graphs
+
+        private ApplicationDbContext _context;
+        public GraphsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Graphs()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ MIS497FinalProject/Models/Graphs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS497FinalProject.Models
+{
+    public class Graphs
+    {
+        public int Protein { get; set; }
+        public int Carbs { get; set; }
+        public int Fat { get; set; }
+    }
+}
