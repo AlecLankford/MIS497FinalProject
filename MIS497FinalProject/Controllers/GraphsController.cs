44063482fe8b270aabad547902606c670590ccb4
Alec Lankford <adlankford@crimson.ua.edu>
2018-11-29 17:37:16 -0600

    Graphs

--- MIS497FinalProject/Controllers/GraphsController.cs
+++ MIS497FinalProject/Controllers/GraphsController.cs
@@ -27,7 +27,8 @@ namespace MIS497FinalProject.Controllers
         {
             var fList = _context.Food.ToList();
             int fCount = fList.Count();
-
+            
+            //nutritionalChart data
             var fatTotal = _context.Food.Sum(f => f.Fat);
             ViewBag.FatTotal = fatTotal;
 
@@ -36,7 +37,32 @@ namespace MIS497FinalProject.Controllers
 
             var carbTotal = _context.Food.Sum(c => c.Carbs);
             ViewBag.CarbTotal = carbTotal;
-            //var carbList
+
+
+            //calorieChart data
+
+
+            var nameArr = _context.Food.Select(f => f.FoodName).ToArray();
+            var calArr = _context.Food.Select(c => c.Calories).ToArray();
+            ViewBag.NameData = nameArr;
+            ViewBag.CalData = calArr;
+
+            //String[,] calChartArr = new string[nameArr.Length, calArr.Length];
+
+            //for (int i = 0; i > 0; i++)
+            //{
+            //    foreach (string name in nameArr)
+            //    {
+            //        calChartArr[i, 0] = name;
+            //    }
+            //}
+
+            //for (int i = 0; i > 0; i++)
+            //{
+            //    foreach (double calorieRecord in calArr)
+            //    {
+            //        calChartArr[0, i] = calorieRecord.ToString();
+            //    }
             
             return View();
         }
--- MIS497FinalProject/Models/Graphs.cs
+++ MIS497FinalProject/Models/Graphs.cs
@@ -7,7 +7,7 @@ namespace MIS497FinalProject.Models
 {
     public class Graphs
     {
-        public int gfoodID { get; set; }
+        public int gfoodName { get; set; }
         public int protein { get; set; }
         public int carbs { get; set; }
         public int fat { get; set; }
