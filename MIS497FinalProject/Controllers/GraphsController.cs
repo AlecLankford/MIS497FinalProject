34bb0860456c1029429af3169cd92b44e0244f07
Alec Lankford <adlankford@crimson.ua.edu>
2018-12-01 11:30:35 -0600

    bar chart

--- MIS497FinalProject/Controllers/GraphsController.cs
+++ MIS497FinalProject/Controllers/GraphsController.cs
@@ -24,10 +24,7 @@ namespace MIS497FinalProject.Controllers
         }
         
         public ActionResult Graphs()
-        {
-            var fList = _context.Food.ToList();
-            int fCount = fList.Count();
-            
+        {   
             //nutritionalChart data
             var fatTotal = _context.Food.Sum(f => f.Fat);
             ViewBag.FatTotal = fatTotal;
@@ -37,44 +34,30 @@ namespace MIS497FinalProject.Controllers
 
             var carbTotal = _context.Food.Sum(c => c.Carbs);
             ViewBag.CarbTotal = carbTotal;
-
-
-            //calorieChart data
-
-
-            var nameArr = _context.Food.Select(f => f.FoodName).ToArray();
-            var calArr = _context.Food.Select(c => c.Calories).ToArray();
-            ViewBag.NameData = nameArr;
-            ViewBag.CalData = calArr;
-
-            //String[,] calChartArr = new string[nameArr.Length, calArr.Length];
-
-            //for (int i = 0; i > 0; i++)
-            //{
-            //    foreach (string name in nameArr)
-            //    {
-            //        calChartArr[i, 0] = name;
-            //    }
-            //}
-
-            //for (int i = 0; i > 0; i++)
-            //{
-            //    foreach (double calorieRecord in calArr)
-            //    {
-            //        calChartArr[0, i] = calorieRecord.ToString();
-            //    }
             
             return View();
         }
+
 
         public ActionResult ReportsHome()
         {
             return View();
         }
 
+
         public ActionResult CalorieChart()
         {
+            var fatTotal = _context.Food.Sum(f => f.Fat);
+            ViewBag.bFatTotal = fatTotal;
+
+            var proteinTotal = _context.Food.Sum(p => p.Protein);
+            ViewBag.bProteinTotal = proteinTotal;
+
+            var carbTotal = _context.Food.Sum(c => c.Carbs);
+            ViewBag.bCarbTotal = carbTotal;
+
             return View();
         }
+
     }
 }
