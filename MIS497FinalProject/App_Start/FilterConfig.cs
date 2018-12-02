01b64ab0b086939a7e90c54eb8fcc00b3efd6db7
Alexandria Norman <adnorman2@crimson.ua.edu>
2018-12-01 19:14:19 -0800

    Re-enabled authorization

--- MIS497FinalProject/App_Start/FilterConfig.cs
+++ MIS497FinalProject/App_Start/FilterConfig.cs
@@ -8,7 +8,7 @@ namespace MIS497FinalProject
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            //filters.Add(new AuthorizeAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
